6600df2d1887d12569158c9f8af39df5ab50c5f5
Ian Banda <ian.d.banda@gmail.com>
2020-05-08 16:36:38 +0800

    fixed font loading bug

--- pages/post/test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ThemeWrapper } from '../../components/Components';
-const Test = () => (
-  <ThemeWrapper>
-    <p>Hello World</p>
-  </ThemeWrapper>
-);
-
-export default Test;
